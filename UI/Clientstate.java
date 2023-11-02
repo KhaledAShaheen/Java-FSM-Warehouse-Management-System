6336862cec02874fd52f28d9ad562049bc63ad7f
khaled shaheen <kshaheen2003@gmail.com>
2023-11-01 21:22:43 -0500

    client gui

--- UI/Clientstate.java
+++ UI/Clientstate.java
@@ -26,54 +26,113 @@ public class Clientstate extends WarehouseState {
     }
 
     public void showClientDetails() {
+        // Assuming WarehouseContext.getUser() returns a String ID
+        String clientId = WarehouseContext.instance().getUser();
         Iterator<Client> allClients = warehouse.getClients();
 
         while (allClients.hasNext()) {
-            Client client = (Client) (allClients.next());
-            if (client.getClientID().equals(WarehouseContext.instance().getUser())) {
-                System.out.println(client);
+            Client client = allClients.next();
+            if (client.getClientID().equals(clientId)) {
+                // Assuming Client class overrides toString to provide meaningful information
+                JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(),
+                        "Client Details:\n" + client,
+                        "Client Information",
+                        JOptionPane.INFORMATION_MESSAGE);
                 return;
             }
         }
+        // If no client matches, inform the user
+        JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(),
+                "No details found for the client with ID: " + clientId,
+                "Client Not Found",
+                JOptionPane.ERROR_MESSAGE);
     }
 
     public void showProducts() {
         Iterator<Product> allProducts = warehouse.getProducts();
-        if (allProducts.hasNext() == false) {
-            System.out.println("No products to print");
+        // Check if there are no products to display
+        if (!allProducts.hasNext()) {
+            JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(),
+                    "No products to print", "Product List", JOptionPane.INFORMATION_MESSAGE);
             return;
         }
+
+        // Use StringBuilder to accumulate product details
+        StringBuilder productsInfo = new StringBuilder();
+
         while (allProducts.hasNext()) {
-            Product product = (Product) (allProducts.next());
-            System.out.println("Product ID: " + product.getId() + " Name: " + product.getName() + " Sale Price: "
-                    + product.getSalePrice());
+            Product product = allProducts.next();
+            productsInfo.append("Product ID: ")
+                    .append(product.getId())
+                    .append(", Name: ")
+                    .append(product.getName())
+                    .append(", Sale Price: ")
+                    .append(product.getSalePrice())
+                    .append("\n"); // Add a new line for each product
         }
+
+        // Create a JTextArea to display the products information
+        JTextArea textArea = new JTextArea(productsInfo.toString());
+        textArea.setEditable(false); // Make the text area non-editable
+        JScrollPane scrollPane = new JScrollPane(textArea); // Add text area to scroll pane
+
+        // Show the scroll pane inside a JOptionPane
+        JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(),
+                scrollPane, "Product List", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public void showTransactions() {
         Iterator<Invoice> invoices = warehouse.getInvoiceList(WarehouseContext.instance().getUser());
-        if (invoices.hasNext() == false) {
-            System.out.println("No invoices to print");
+
+        // Check if there are no invoices to display
+        if (!invoices.hasNext()) {
+            JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(),
+                    "No invoices to print", "Transaction List", JOptionPane.INFORMATION_MESSAGE);
             return;
         }
+
+        // Use StringBuilder to accumulate invoice details
+        StringBuilder invoiceInfo = new StringBuilder();
+
         while (invoices.hasNext()) {
-            Invoice invoice = (Invoice) (invoices.next());
-            System.out.println(invoice);
+            Invoice invoice = invoices.next();
+            // Assuming Invoice class has a meaningful toString implementation
+            invoiceInfo.append(invoice.toString()).append("\n");
         }
+
+        // Create a JTextArea to display the invoices information
+        JTextArea textArea = new JTextArea(invoiceInfo.toString());
+        textArea.setEditable(false); // Make the text area non-editable
+        JScrollPane scrollPane = new JScrollPane(textArea); // Add text area to a scroll pane
+
+        // Show the scroll pane inside a JOptionPane
+        JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(),
+                scrollPane, "Transaction List", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public void showPayments() {
         Iterator<Double> payments = warehouse.getPayments(WarehouseContext.instance().getUser());
-        int num = 1;
-        if (payments.hasNext() == false) {
-            System.out.println("No payments to print");
+        int num = 1; // Counter for numbering the payments
+        StringBuilder paymentsInfo = new StringBuilder();
+
+        if (!payments.hasNext()) {
+            JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(),
+                    "No payments to print", "Payment List", JOptionPane.INFORMATION_MESSAGE);
             return;
         }
+
         while (payments.hasNext()) {
-            Double payment = (Double) (payments.next());
-            System.out.println("amount " + num + ": " + payment);
+            Double payment = payments.next();
+            paymentsInfo.append("Payment ").append(num).append(": ").append(payment).append("\n");
             num++;
         }
+
+        JTextArea textArea = new JTextArea(paymentsInfo.toString());
+        textArea.setEditable(false); // The text area should not be editable by the user
+        JScrollPane scrollPane = new JScrollPane(textArea); // Make the text area scrollable
+
+        JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(),
+                scrollPane, "Payment List", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public void cartState() {
@@ -82,15 +141,31 @@ public class Clientstate extends WarehouseState {
 
     public void showProductsInWaitlist() {
         Iterator<Hold> waitlist = warehouse.getWaitListByClientId(WarehouseContext.instance().getUser());
-        if (waitlist == null || waitlist.hasNext() == false) {
-            System.out.println("No products in waitlist to print");
+
+        // Check if the waitlist is empty or null
+        if (waitlist == null || !waitlist.hasNext()) {
+            JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(),
+                    "No products in waitlist to print", "Waitlist", JOptionPane.INFORMATION_MESSAGE);
             return;
         }
+
+        // Use a StringBuilder to accumulate the hold details
+        StringBuilder waitlistInfo = new StringBuilder();
+
         while (waitlist.hasNext()) {
-            Hold hold = (Hold) (waitlist.next());
-            System.out.println(hold);
+            Hold hold = waitlist.next();
+            // Assuming Hold class has a meaningful toString implementation
+            waitlistInfo.append(hold.toString()).append("\n");
         }
 
+        // Create a JTextArea to display the waitlist information
+        JTextArea textArea = new JTextArea(waitlistInfo.toString());
+        textArea.setEditable(false); // Make the text area non-editable
+        JScrollPane scrollPane = new JScrollPane(textArea); // Add text area to a scroll pane
+
+        // Show the scroll pane inside a JOptionPane
+        JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(),
+                scrollPane, "Waitlist", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public void run() {
