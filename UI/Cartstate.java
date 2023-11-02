494f29b52b6d2f0b01b3fc37c1bfb1ceeb3ea16a
khaled shaheen <kshaheen2003@gmail.com>
2023-11-01 22:05:24 -0500

    finish

--- UI/Cartstate.java
+++ UI/Cartstate.java
@@ -31,34 +31,35 @@ public class Cartstate extends WarehouseState {
     }
 
     public void showProductsInWishlist() {
-        JFrame frame = WarehouseContext.instance().getFrame();
-        frame.getContentPane().removeAll();
-        frame.setTitle("Wishlist");
+        Iterator<Record> records = warehouse.getWishList(WarehouseContext.instance().getUser());
+        if (!records.hasNext()) {
+            JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(), "No products to show");
+            return;
+        }
 
-        // Text area for displaying records
-        JTextArea textArea = new JTextArea(15, 30);
-        textArea.setEditable(false); // make textArea non-editable
+        // Use a StringBuilder to accumulate product strings
+        StringBuilder productListBuilder = new StringBuilder();
+        while (records.hasNext()) {
+            Record record = records.next();
+            productListBuilder.append("Product ID: ").append(record.getProduct().getId())
+                    .append(" Name: ").append(record.getProduct().getName())
+                    .append(" Sale Price: ").append(record.getProduct().getSalePrice())
+                    .append(" Amount: ").append(record.getQuantity())
+                    .append("\n");
+        }
+
+        // Create a JTextArea to display your products
+        JTextArea textArea = new JTextArea(20, 50);
+        textArea.setText(productListBuilder.toString());
+        textArea.setEditable(false); // Make it read-only
+
+        // Wrap JTextArea inside a JScrollPane in case of overflow
         JScrollPane scrollPane = new JScrollPane(textArea);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-        // Fetch records and display them in the text area
-        Iterator<Record> records = warehouse.getWishList(WarehouseContext.instance().getUser());
-        if (!records.hasNext()) {
-            textArea.setText("No records to print");
-        } else {
-            StringBuilder sb = new StringBuilder();
-            while (records.hasNext()) {
-                Record record = records.next();
-                sb.append(record.toString()).append("\n");
-            }
-            textArea.setText(sb.toString());
-        }
-
-        // Add components to frame
-        frame.getContentPane().add(scrollPane);
-        frame.pack(); // Adjusts window to the contents
-        frame.setLocationRelativeTo(null); // Center the window
-        frame.setVisible(true);
+        // Display the JScrollPane in a JOptionPane
+        JOptionPane.showMessageDialog(WarehouseContext.instance().getFrame(), scrollPane, "Product List",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 
     public void addProduct() {
@@ -143,7 +144,7 @@ public class Cartstate extends WarehouseState {
         }
 
         // Attempt to change the product quantity
-        boolean result = warehouse.editProductQuantity(productId, WarehouseContext.instance().getUser(), newQuantity);
+        boolean result = warehouse.editProductQunatity(productId, WarehouseContext.instance().getUser(), newQuantity);
         if (result) {
             JOptionPane.showMessageDialog(frame, "Edit Successful!", "Success", JOptionPane.INFORMATION_MESSAGE);
         } else {
